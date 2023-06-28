b2e0fc81655847bde368f53eac9ef075440768d8
Amatus muscheites <amatus.muscheites@gmail.com>
2023-06-28 10:02:09 +0200

    add failing test

---
+++ data-ms/src/test/java/com/example/DataMsSampleTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class DataMsSampleTest {
+
+    @Test
+    void failingTest() {
+        fail();
+    }
+}
