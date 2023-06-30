9c7bbd5810b2f640cf9c1c1bd6a0a3114feaf6b8
Amatus muscheites <amatus.muscheites@gmail.com>
2023-06-30 10:51:53 +0200

    re-add object id

--- mongodb-service/src/main/java/com/example/person/Person.java
+++ mongodb-service/src/main/java/com/example/person/Person.java
@@ -1,11 +1,13 @@
 package com.example.person;
 
 import io.quarkus.mongodb.panache.common.MongoEntity;
+import org.bson.types.ObjectId;
 
 import java.time.LocalDate;
 
 @MongoEntity
 public class Person {
+    public ObjectId _id;
     public String name;
     public LocalDate birth;
 }
